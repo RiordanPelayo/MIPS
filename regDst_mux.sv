1148fadb3da794f2709b7ca87fec57b8e4e872c1
Riordan <riordanpelayo@gmail.com>
2020-04-25 11:11:49 -0500

    regDst mux fixed to resive inmediate data

--- regDst_mux.sv
+++ regDst_mux.sv
@@ -1,11 +1,14 @@
-module regDst_mux #(parameter INST = 5)
+module regDst_mux 
 (
-    input  reg  [(INST-1):0] Reg2,        //Reg2 Data
-    input  reg  [(INST-1):0] Inmediate,   //Inmediate Data
-    input  reg  RegDst,                   //Bit of Control
-    output wire [(INST-1):0] WR           //Write Register
+    input  reg  [4:0]  Reg2,        //Reg2 Data
+    input  reg  [15:0] Inmediate,   //Inmediate Data
+    input  reg  RegDst,             //Bit of Control
+    output wire [4:0] WR            //Write Register
 );
 
+reg [4:0] number; //Register to save the inmediate value
+
+    assign number =  Inmediate[15:11];    //Take only the inmediate value
     assign WR = !RegDst ? Reg2:Inmediate; //multiplexer
       
 endmodule
